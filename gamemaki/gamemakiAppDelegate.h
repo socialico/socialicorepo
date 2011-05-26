7bb6804deb0fcf993e72099e264bbbc17a084a0f
socialico <iphone@socialico.com>
2011-05-26 14:40:50 +0800

    Initial Commit

---
+++ gamemaki/gamemakiAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  gamemakiAppDelegate.h
+//  gamemaki
+//
+//  Created by Damon Widjaja on 5/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gamemakiAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end

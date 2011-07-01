c057197a83ec75118b29f97ef2b85dd66376f8c4
Steve Sng <steve@socialico.com>
2011-07-01 12:07:31 +0800

    added fb login

---
+++ gamemaki/HomeController.h
@@ -0,0 +1,16 @@
+//
+//  HomeController.h
+//  gamemaki
+//
+//  Created by Socialico on 6/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface HomeController : NSObject {
+    
+}
+
+@end
---
+++ gamemaki/HomeController.m
@@ -0,0 +1,14 @@
+//
+//  HomeController.m
+//  gamemaki
+//
+//  Created by Socialico on 6/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HomeController.h"
+
+
+@implementation HomeController
+
+@end
